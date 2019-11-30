06a81b8e495511f4af5626b2b00a616ff2da6b07
OZONE4REAL <ezenwaogbonna1@gmail.com>
2019-11-30 07:07:15 +0100

    add elastic search docker image to circleci config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -46,6 +46,20 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.before(:suite) do
+    # reindex models
+    Product.reindex
+
+    # and disable callbacks
+    Searchkick.disable_callbacks
+  end
+
+  config.around(:each, search: true) do |example|
+    Searchkick.callbacks(true) do
+      example.run
+    end
+  end
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
